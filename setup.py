ae5f067c092e946d6cb07124a8772abf6bd8cfb1
Alan Murray <github@atmurray.net>
2020-03-23 21:40:53 +1100

    Updated setup.py, pre-commit hooks

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 from distutils.core import setup
+
 setup(
-  name = 'aiopulse',
-  packages = ['aiopulse'],
-  version = '0.1', 
-  license='apache-2.0',
-  description = 'Python module to talk to Rollease Acmeda Automate Pulse Hub roller bind using asyncio.',
-  author = 'Alan Murray',
-  author_email = 'pypi@atmurray.net',
-  url = 'https://github.com/atmurray/aiopulse',
-  download_url = 'https://github.com/user/aiopulse/archive/v_01.tar.gz',
-  keywords = ['automation'],
-  install_requires=[
-          'asyncio',
-          'async_timeout',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Operating System :: OS Independent',
-    'Topic :: Software Development :: Libraries :: Python Modules'
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-  ],
+    name="aiopulse",
+    packages=["aiopulse"],
+    version="0.1",
+    license="apache-2.0",
+    description="Python module to talk to Rollease Acmeda Automate Pulse Hub roller bind using asyncio.",
+    author="Alan Murray",
+    author_email="pypi@atmurray.net",
+    url="https://github.com/atmurray/aiopulse",
+    download_url="https://github.com/atmurray/aiopulse/archive/v0.1.tar.gz",
+    keywords=["automation"],
+    install_requires=["asyncio", "async_timeout",],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        "License :: OSI Approved :: Apache Software License v2.0",
+        "Programming Language :: Python :: 3",
+    ],
 )
