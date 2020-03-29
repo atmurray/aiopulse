a359f6cf624952f756f053cf390f4d21bbc82de2
Alan Murray <github@atmurray.net>
2020-03-29 18:04:42 +1100

    Fixed exported module classes.

--- aiopulse/__init__.py
+++ aiopulse/__init__.py
@@ -1,4 +1,11 @@
 """Rollease Acmeda Automate Pulse asyncio protocol implementation."""
 import logging
 
+from aiopulse.hub import Hub
+from aiopulse.elements import Roller, Room, Scene
+
+__all__ = ["Hub", "Roller", "Room", "Scene"]
+__version__ = "0.2.1"
+__author__ = "Alan Murray"
+
 _LOGGER = logging.getLogger(__name__)
