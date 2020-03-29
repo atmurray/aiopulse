e0c731557ccea16121968b0112eea7d0b8593bce
Alan Murray <github@atmurray.net>
2020-03-29 17:44:12 +1100

    Release of version 0.2 Published to https://pypi.org/project/aiopulse/0.2/

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ from distutils.core import setup
 setup(
     name="aiopulse",
     packages=["aiopulse"],
-    version="0.1",
+    version="0.2",
     license="apache-2.0",
     description="Python module for Rollease Acmeda Automate integration.",
     author="Alan Murray",
     author_email="pypi@atmurray.net",
     url="https://github.com/atmurray/aiopulse",
-    download_url="https://github.com/atmurray/aiopulse/archive/v0.1.tar.gz",
+    download_url="https://github.com/atmurray/aiopulse/archive/v0.2.tar.gz",
     keywords=["automation"],
     install_requires=["asyncio", "async_timeout"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules"
+        "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
     ],
