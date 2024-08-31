4483f4488778f6ac5b1d4c219b717135a5e16a75
Alan Murray <github@atmurray.net>
2024-08-31 13:38:43 +1000

    Fix missing reference to Timer object

--- aiopulse/__init__.py
+++ aiopulse/__init__.py
@@ -5,6 +5,7 @@ from aiopulse.hub import Hub
 from aiopulse.roller import Roller
 from aiopulse.room import Room
 from aiopulse.scene import Scene
+from aiopulse.timer import Timer
 from aiopulse.errors import (
     CannotConnectException,
     NotConnectedException,
@@ -18,13 +19,14 @@ __all__ = [
     "Roller",
     "Room",
     "Scene",
+    "Timer",
     "CannotConnectException",
     "NotConnectedException",
     "NotRunningException",
     "InvalidResponseException",
     "UpdateType",
 ]
-__version__ = "0.4.5"
+__version__ = "0.4.6"
 __author__ = "Alan Murray"
 
 _LOGGER = logging.getLogger(__name__)
