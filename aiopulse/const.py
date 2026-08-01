7871f2375ba65326c5894e41feb46c8d6ff0a665
Alan Murray <github@atmurray.net>
2026-08-01 16:59:27 +1000

    refactor: organize protocol constants with enums

--- aiopulse/const.py
+++ aiopulse/const.py
@@ -1,9 +1,66 @@
 """Acmeda Pulse Hub constants."""
-from enum import Enum
+from enum import Enum, IntEnum
 
-UpdateType = Enum("UpdateType", "info rollers rooms scenes timers")
+
+class UpdateType(Enum):
+    """Types of hub updates."""
+    info = "info"
+    rollers = "rollers"
+    rooms = "rooms"
+    scenes = "scenes"
+    timers = "timers"
+
+
+class MessageType(IntEnum):
+    """Protocol message types for incoming messages."""
+    HUB_INFO = 0x1600
+    HUB_INFO_UPDATED = 0x0D00
+    ROOM_LIST = 0x0101
+    SCENE_LIST = 0x3301
+    ROLLER_LIST = 0x2101
+    TIMER_LIST = 0x4101
+    AUTH_INFO = 0x0800
+    POSITION = 0x2301
+    ROLLER_UPDATED = 0x2501
+    TIMER_CREATED = 0x4301
+    TIMER_DEVICE_UPDATED = 0x4501
+    TIMER_INFO_UPDATED = 0x4901
+    TIMER_DELETED = 0x4701
+    ROLLER_HEALTH = 0x2B01
+    DISCOVER_RESPONSE = 0x0F00
+
+
+class CommandType(IntEnum):
+    """Command message types."""
+    DISCOVER = 0x03000003
+    CONNECT = 0x03000006
+    LOGIN = 0x0F000008
+    PING = 0x03000015
+    SETID = 0x28000090
+    UNKNOWN1 = 0x23000090
+    GET_HUB_INFO = 0x1E000090
+    MOVE_TO = 0x34000090
+    MOVE = 0x2D000090
+    GET_HEALTH = 0x32000090
+
+
+class ResponseType(IntEnum):
+    """Response message types."""
+    DISCOVER = 0x57000004
+    CONNECT = 0x0F000007
+    LOGIN = 0x04000009
+    PING = 0x03000016
+    SETID = 0x03000091
+    UNKNOWN1 = 0x28000091
+    GET_HUB_INFO = 0x4A000091
+    MOVE_TO = 0x34000091
+    GET_ROOMS = 0x01000091
+    GET_ROLLERS = 0x03000091
+
 
 HEADER = bytes.fromhex("00000003")
+
+# Keep old constants for backward compatibility
 COMMAND_DISCOVER = bytes.fromhex("03000003")
 RESPONSE_DISCOVER = bytes.fromhex("57000004")
 
--- tests/test_const.py
+++ tests/test_const.py
@@ -2,6 +2,9 @@ import pytest
 
 from aiopulse.const import (
     UpdateType,
+    MessageType,
+    CommandType,
+    ResponseType,
     HEADER,
     COMMAND_DISCOVER,
     RESPONSE_DISCOVER,
@@ -26,20 +29,52 @@ from aiopulse.const import (
 )
 
 
-class TestUpdateType:
-    def test_enum_values(self):
-        assert UpdateType.info.value == 1
-        assert UpdateType.rollers.value == 2
-        assert UpdateType.rooms.value == 3
-        assert UpdateType.scenes.value == 4
-        assert UpdateType.timers.value == 5
+class TestMessageType:
+    def test_message_type_values(self):
+        assert MessageType.HUB_INFO == 0x1600
+        assert MessageType.ROOM_LIST == 0x0101
+        assert MessageType.ROLLER_LIST == 0x2101
+        assert MessageType.SCENE_LIST == 0x3301
+        assert MessageType.TIMER_LIST == 0x4101
+        assert MessageType.POSITION == 0x2301
+        assert MessageType.ROLLER_UPDATED == 0x2501
+        assert MessageType.ROLLER_HEALTH == 0x2B01
 
-    def test_enum_members(self):
-        assert UpdateType(1) == UpdateType.info
-        assert UpdateType(2) == UpdateType.rollers
-        assert UpdateType(3) == UpdateType.rooms
-        assert UpdateType(4) == UpdateType.scenes
-        assert UpdateType(5) == UpdateType.timers
+    def test_message_type_is_int(self):
+        assert isinstance(MessageType.HUB_INFO, int)
+
+
+class TestCommandType:
+    def test_command_type_values(self):
+        assert CommandType.DISCOVER == 0x03000003
+        assert CommandType.CONNECT == 0x03000006
+        assert CommandType.LOGIN == 0x0F000008
+        assert CommandType.PING == 0x03000015
+        assert CommandType.SETID == 0x28000090
+        assert CommandType.GET_HUB_INFO == 0x1E000090
+        assert CommandType.MOVE_TO == 0x34000090
+        assert CommandType.MOVE == 0x2D000090
+        assert CommandType.GET_HEALTH == 0x32000090
+
+
+class TestResponseType:
+    def test_response_type_values(self):
+        assert ResponseType.DISCOVER == 0x57000004
+        assert ResponseType.CONNECT == 0x0F000007
+        assert ResponseType.LOGIN == 0x04000009
+        assert ResponseType.PING == 0x03000016
+        assert ResponseType.SETID == 0x03000091
+        assert ResponseType.GET_HUB_INFO == 0x4A000091
+        assert ResponseType.MOVE_TO == 0x34000091
+
+
+class TestUpdateType:
+    def test_update_type_values(self):
+        assert UpdateType.info.name == "info"
+        assert UpdateType.rollers.name == "rollers"
+        assert UpdateType.rooms.name == "rooms"
+        assert UpdateType.scenes.name == "scenes"
+        assert UpdateType.timers.name == "timers"
 
 
 class TestConstants:
